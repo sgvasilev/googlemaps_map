3666a25b00850687f54550423a023677296ff43d
Sergey <sendto.vasilevsg@gmail.com>
2022-05-26 19:53:22 +0300

    fixed server

--- test.js
+++
@@ -1,8 +0,0 @@
-const latLng = { lat: () => 39, lng: () => 39 }
-const getPlacesDetails = () => {
-  return new Promise((res, rej) => {
-    process.nextTick(() => res(latLng))
-  })
-}
-let data = getPlacesDetails()
-data.then((res) => console.log(res.lat()))
